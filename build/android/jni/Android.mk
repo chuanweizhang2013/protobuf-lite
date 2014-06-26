d95058d12e777fd6d96cd15c175ee1aa89cd5f54
chuanweizhang2013 <chuanweizhang2013@gmail.com>
2014-06-26 02:01:40 -0700

    add adnroid lib

--- build/android/jni/Android.mk
+++ build/android/jni/Android.mk
@@ -1,5 +1,4 @@
 LOCAL_PATH := $(call my-dir)  
-APP_ABI :=armeabi armeabi-v7a x86
 
 include $(CLEAR_VARS)  
   
@@ -9,20 +8,19 @@ LOCAL_MODULE_FILENAME := libprotobuf-lite
    
 LOCAL_SRC_FILES := \
 ../../../src/google/protobuf/io/coded_stream.cc \
-../../../srcgoogle/protobuf/stubs/common.cc \
-../../../srcgoogle/protobuf/extension_set.cc \
-../../../srcgoogle/protobuf/generated_message_util.cc \
-../../../srcgoogle/protobuf/message_lite.cc \
-../../../srcgoogle/protobuf/stubs/once.cc \
-../../../srcgoogle/protobuf/repeated_field.cc \
-../../../srcgoogle/protobuf/wire_format_lite.cc \
-../../../srcgoogle/protobuf/io/zero_copy_stream.cc \
-../../../srcgoogle/protobuf/io/zero_copy_stream_impl_lite.cc
+../../../src/google/protobuf/stubs/common.cc \
+../../../src/google/protobuf/extension_set.cc \
+../../../src/google/protobuf/generated_message_util.cc \
+../../../src/google/protobuf/message_lite.cc \
+../../../src/google/protobuf/stubs/once.cc \
+../../../src/google/protobuf/repeated_field.cc \
+../../../src/google/protobuf/wire_format_lite.cc \
+../../../src/google/protobuf/io/zero_copy_stream.cc \
+../../../src/google/protobuf/io/zero_copy_stream_impl_lite.cc
 
 LOCAL_EXPORT_C_INCLUDES :=
 LOCAL_EXPORT_LDLIBS :=
 LOCAL_C_INCLUDES :=\
-$(LOCAL_PATH) \
 $(LOCAL_PATH)/../../../src
   
 LOCAL_LDLIBS := \
