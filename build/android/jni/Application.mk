237e6bd5c9f5f5c51ee71e069299a87bbdb5c3f3
chuanweizhang2013 <chuanweizhang2013@gmail.com>
2014-06-26 04:12:00 -0700

    android lib

--- build/android/jni/Application.mk
+++ build/android/jni/Application.mk
@@ -2,5 +2,4 @@ APP_MODULES := protobuf-lite_static
 APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -std=c++11 -fsigned-char
 APP_CPPFLAGS += -fexceptions
-#APP_ABI :=armeabi-v7a
 APP_ABI :=armeabi armeabi-v7a x86
