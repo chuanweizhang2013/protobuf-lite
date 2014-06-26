9e65d531c3955dc286c92359513c5694c683efb3
chuanweizhang2013 <chuanweizhang2013@gmail.com>
2014-06-26 02:21:21 -0700

    Initial Commit

---
+++ build/mac_ios/protobuf/protobuf/protobuf.h
@@ -0,0 +1,13 @@
+//
+//  protobuf.h
+//  protobuf
+//
+//  Created by hujiang on 14-6-26.
+//  Copyright (c) 2014年 hujiang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface protobuf : NSObject
+
+@end
---
+++ build/mac_ios/protobuf/protobuf/protobuf.m
@@ -0,0 +1,13 @@
+//
+//  protobuf.m
+//  protobuf
+//
+//  Created by hujiang on 14-6-26.
+//  Copyright (c) 2014年 hujiang. All rights reserved.
+//
+
+#import "protobuf.h"
+
+@implementation protobuf
+
+@end
